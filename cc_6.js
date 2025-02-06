f0c908ea20a3e4bcb31785b376fd4e155f542347
Markayla Montgomery <mm42@usf.edu>
2025-02-05 20:29:55 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -22,3 +22,26 @@ return salesTax;
 };
 calculateSalesTax(100, 0.07);
 calculateSalesTax(500, 0.1);
+
+// Task 3: Arrow Function
+// Write an arrow function calculateBonus(salary, performanceRating) to determine a bonus:
+const calculateBonus = (salary, performanceRating) => {
+    // bonus variable
+    let bonus;
+    // Determine bonus based on performance rating
+    if (performanceRating === "Excellent") {
+      bonus = salary * 0.20;
+    } else if (performanceRating === "Good") {
+      bonus = salary * 0.10;
+    } else if (performanceRating === "Average") {
+      bonus = salary * 0.05;
+    } else {
+      bonus = 0; // If performance rating is unknown
+    }
+    // Log the result
+    console.log(`Bonus: $${bonus}`);
+    // Return the bonus
+    return bonus;
+  };
+  calculateBonus(5000, "Excellent");
+  calculateBonus(7000, "Good");
